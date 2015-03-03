9e9d95b4b589ce6105af8322ad8e62bdd28ed629
luciofornarini <luciofornarini@gmail.com>
2015-03-03 16:40:39 -0300

    luciofornarini
    bootcamp

--- Topic 3/index.js
+++ Topic 3/index.js
@@ -1,3 +1,9 @@
 
-var Movie = required('./movie.js')
-var Director = required('./director.js')
+var Movie = require('./movie.js')
+var Director = require('./director.js')
+
+var alien = new Movie();
+var ridleyScott = new Director('Ridley Scott');
+ridleyScott.set('quotes', ['Cast is everything.', 'Do what ...']);
+alien.set('director', ridleyScott);
+alien.get('director').speak();
--- Topic 3/movie.js
+++ Topic 3/movie.js
@@ -5,15 +5,15 @@ function Movie(){
 	};
 };
 
-module.exports = Movie;
+module.exports = Movie();
 
-var Director = required('./director.js')
+var Director = require('./director.js')
 
 //http://spinejs.com/docs/commonjs
 
 	Movie.prototype.set = function(attr, value){
 				this.attributes[attr] = value;
-			} 
+		} 
 
 	Movie.prototype.get = function(){
 			console.log(this.attributes['titulo'] + this.attributes['fecha']);	
